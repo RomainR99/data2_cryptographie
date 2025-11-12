b734dbae8074919124f49367a31a993c2992e93e
Romain <romain.rothn@gmail.com>
2025-11-12 12:36:58 +0100

    commit -m "feature hack_cesar_cipher"

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 def cesar_cipher(text, key):
     crypted_text = ""
     for char in text:
@@ -12,5 +14,13 @@ def cesar_uncipher(crypted_message,key):
 
 messge ="chocolat"
 crypted_message = cesar_cipher(message,5212000)
-initial_message = #on verrifie un bouleen avec True pour voir que le message se reverse bien
+#initial_message = #on verrifie un bouleen avec True pour voir que le message se reverse bien
+
+def hack_cesar_cipher(crypted_message):
+    for possible_key in range(0,1_114_112) #au lieu de mettre 1114112 plus visible
+        possible_uncription = cesar_uncipher(crypted_message, possible_key)
+        if possible_uncryption[0] in string.printabal: #taille de la table string.principal contient 100 élément au lieu de 1_114_112 donc plus rapide à décoder
+            print(possible_key)
+            print(cesar_unicipher(crypted_message, possible_key))#donne tous les décodage possible
+            print(_""*20)
 	
